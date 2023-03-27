9d529560f1b492fd504f2e0ca493db0fc731671d
liyong <liyong@lanren-inc.com>
2023-03-27 15:51:57 +0800

    save

---
+++ server.js
@@ -0,0 +1,20 @@
+const Koa = require('koa');
+const static = require('koa-static');
+const Router = require('koa-router');
+const fs = require('fs');
+const app = new Koa();
+const router = new Router();
+app.use(static(__dirname + '/docs/.vuepress/dist'))
+/*
+router.get('/', (ctx) => {
+    console.log("sss")
+    ctx.response.type = 'html';
+    ctx.response.body = fs.createReadStream('index.html');
+})
+app.use(router.routes());
+app.use(router.allowedMethods());
+*/
+//端口
+app.listen(80, () => {
+    console.log('server is starting at port 80');
+})
